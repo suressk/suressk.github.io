b57697f1b25b66e9fa5efdc881b80fb8e0e6b76e
suressk <sure_ssk@outlook.com>
2022-12-19 02:55:47 +0800

    feat: 🏖 add Tip component

---
+++ docs/components/Tip.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="custom-block" :class="[cls]">
+    <p class="custom-block-title">{{ props.title }}</p>
+    <p>{{ props.content }}</p>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const typeMap = {
+  success: 'tip',
+  danger: 'danger',
+  info: 'info'
+}
+const props = defineProps({
+  type: String,
+  title: String,
+  content: String,
+})
+
+const cls = computed(() => typeMap[props.type] || 'info')
+</script>
